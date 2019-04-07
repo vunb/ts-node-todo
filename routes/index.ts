e8a5392fa38ff267cc1adb4f3736692ee33ebe8b
Vunb <nhubaovu@gmail.com>
2019-04-07 07:41:56 +0700

    xu ly loi ung dung

--- routes/index.ts
+++ routes/index.ts
@@ -1,4 +1,5 @@
-import {Express} from "express";
+import {Express, Request, Response} from "express";
+import { logger } from "../config/logger";
 
 export function routes(app: Express) {
   app.get("/", (req, res) => res.send('Ok'));
@@ -9,5 +10,17 @@ export function routes(app: Express) {
       error: "Endpoint not found"
     });
     next();
+  });
+
+  // Handle application error
+  app.use((error: any, req: Request, res: Response, next:Function) => {
+    logger.error("We got an error!", error);
+
+    if (process.env.NODE_ENV === "production") {
+        return res.status(500).json({
+          error: "Unexpected error: " + error
+        });
+    }
+    next(error);
   });
 }
