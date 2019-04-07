da346a23baae3eb85d2dcb7737b7f0d2e227684c
Vunb <nhubaovu@gmail.com>
2019-04-07 07:37:26 +0700

    start server

--- server.ts
+++ server.ts
@@ -1,3 +1,9 @@
 import { logger } from "./config/logger";
+import { app } from "./config/express";
 
 logger.info('Làm việc với Winston!');
+
+const port = process.env.PORT || 3000;
+const server = app.listen(port, () => {
+  logger.info(`Server start listening on port ${port}!`);
+});
